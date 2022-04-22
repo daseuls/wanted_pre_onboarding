94a8336200d2d63b6734d3ca49ad6fcfb8969832
daseuls <dls2tmfs2@naver.com>
2022-04-22 16:44:19 +0900

    feat: 초기세팅

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+function App() {
+  return <Container className="App">원티드 Pre onboarding 사전 과제</Container>;
+}
+
+export default App;
+
+const Container = styled.div``;
