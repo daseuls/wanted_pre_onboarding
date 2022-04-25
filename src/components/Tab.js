223a632a5eb5f4ba54da3b4a9a139d00cc41b142
daseuls <dls2tmfs2@naver.com>
2022-04-25 09:58:24 +0900

    feat: Tab.js 구현

--- src/components/Tab.js
+++ src/components/Tab.js
@@ -1,20 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 
+const MENU = ["감자", "고구마", "카레라이스"];
+
 export default function Tab() {
-  const MENU = ["감자", "고구마", "카레라이스"];
+  const [itemIndex, setItemIndex] = useState(0);
+
   return (
     <Container>
       <Title>2. Tab</Title>
       <TabContainer>
         <MenuContainer>
-          {MENU.map((el) => (
-            <MenuTitle>{el}</MenuTitle>
+          {MENU.map((el, index) => (
+            <MenuTitle
+              onClick={() => setItemIndex(index)}
+              isSelected={itemIndex === index}
+              itemLength={100 / MENU.length}
+            >
+              {el}
+            </MenuTitle>
           ))}
         </MenuContainer>
         <TabBarContainer>
           <TabBar></TabBar>
-          <TabSlider></TabSlider>
+          <TabSlider
+            itemIndex={itemIndex}
+            itemLength={100 / MENU.length}
+          ></TabSlider>
         </TabBarContainer>
       </TabContainer>
     </Container>
@@ -28,24 +40,42 @@ const Title = styled.h2``;
 const TabContainer = styled.div`
   display: flex;
   flex-direction: column;
+  width: 400px;
 `;
 
-const MenuTitle = styled.p``;
+const MenuTitle = styled.div`
+  width: ${(props) => props.itemLength}%;
+  justify-content: center;
+  display: flex;
+  padding: 15px;
+  font-size: 14px;
+  cursor: pointer;
+  color: ${(props) => (props.isSelected ? "black" : "gray")};
+  font-weight: 700;
+`;
 
 const MenuContainer = styled.div`
   display: flex;
+  width: 100%;
 `;
 
 const TabBarContainer = styled.div`
-  width: 300px;
-  /* height: 10px; */
-  /* background-color: red; */
+  position: relative;
+  width: 100%;
 `;
 
 const TabBar = styled.div`
   width: 100%;
-  height: 2px;
-  background-color: gray;
+  height: 2.5px;
+  background-color: lightgray;
 `;
 
-const TabSlider = styled.div``;
+const TabSlider = styled.div`
+  position: absolute;
+  height: 2.5px;
+  top: 0;
+  background-color: #6ebfb8;
+  width: ${(props) => props.itemLength}%;
+  transform: translateX(${(props) => props.itemIndex * 100}%);
+  transition: all 0.3s ease;
+`;
